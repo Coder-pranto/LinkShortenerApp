b9b26f8610ba66a14cb146ea86351d24da2b1822
Riaj Hasan Pranto <prantocse18@gmail.com>
2026-04-09 15:22:43 +0600

    fix: update BASE_URL to use relative path and clean up API endpoint calls

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = '/';
 
 const api = axios.create({
     baseURL: BASE_URL,
 })
 
 
-export const createShortUrl = (originalUrl) => api.post('/shorten', {originalUrl});
-export const redirectToOriginalUrl = (code) =>window.open(`${BASE_URL}/${code}`, '_blank');
-export const getUrlStats = (code) =>api.get(`/stats/${code}`);
+export const createShortUrl = (originalUrl) => api.post('shorten', {originalUrl});
+export const redirectToOriginalUrl = (code) =>window.open(`${BASE_URL}${code}`, '_blank');
+export const getUrlStats = (code) =>api.get(`stats/${code}`);
   
