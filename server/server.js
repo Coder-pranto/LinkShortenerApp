7cb1b83ca6dd595c5a57e1885ebe015d1864776e
Riaj Hasan Pranto <prantocse18@gmail.com>
2026-04-09 15:20:04 +0600

    fix: update Vite React plugin dependency and correct CORS origin URL

--- server/server.js
+++ server/server.js
@@ -32,8 +32,8 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === 'production'
-        ? 'https://linkshortener.onrender.com'
-        :  process.env.CLIENT_URL,
+        ? 'https://linkshortenerapp.onrender.com'
+        : process.env.CLIENT_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
